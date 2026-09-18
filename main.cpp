b76b1ee2000cb66682c6418d01ddb6be2db8b34a
Nguyen Chi Bao <ncbslav@gmail.com>
2026-09-18 09:22:14 +0700

    practice 1 for week 8

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string fullName, studentID, email, hometown;
+
+    cout << "Enter your full name: ";
+    getline(cin, fullName);
+
+    cout << "Enter your student ID: ";
+    getline(cin, studentID);
+
+    cout << "Enter your email: ";
+    getline(cin, email);
+
+    cout << "Enter your hometown: ";
+    getline(cin, hometown);
+
+    cout << "\n===== STUDENT PROFILE =====\n";
+    cout << "Full name   : " << fullName << endl;
+    cout << "Student ID  : " << studentID << endl;
+    cout << "Email       : " << email << endl;
+    cout << "Hometown    : " << hometown << endl;
+    cout << "===========================\n";
+
+    return 0;
+}
